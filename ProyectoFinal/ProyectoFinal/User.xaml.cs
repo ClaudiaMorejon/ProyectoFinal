95b2bd1dbd624fb45606ef68c2e34e86794969da
BYRON SANTIAGO <byronresort@hotmail.com>
2020-11-25 02:07:23 -0500

    interfaz y botones user y userprofile

--- ProyectoFinal/ProyectoFinal/User.xaml.cs
+++ ProyectoFinal/ProyectoFinal/User.xaml.cs
@@ -10,52 +10,42 @@ using System.Net;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-
 namespace ProyectoFinal
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class UserDataProfile : ContentPage
+    public partial class User : ContentPage
     {
-        public UserDataProfile()
+        public User()
         {
             InitializeComponent();
         }
+
+        
 
         private async void btnGuardar_Clicked(object sender, EventArgs e)
         {
 
             try
             {
-
                 WebClient UserProfile = new WebClient();
                 var parametros = new System.Collections.Specialized.NameValueCollection();
-                parametros.Add("idPerson", idPerson.Text);
-                parametros.Add("idUser", idUser.Text);
-                parametros.Add("nombre", Nombre.Text);
-                parametros.Add("apellido", Apellido.Text);
-                parametros.Add("cedula", Cedula.Text);
-                parametros.Add("celular", Celular.Text );
-                parametros.Add("convencional", Convencional.Text );
-                parametros.Add("direccion", Direccion.Text);
-                parametros.Add("ciudad", Ciudad.Text );
-                parametros.Add("imagen", Foto.Text);
-                parametros.Add("genero", Genero.Text);
+                parametros.Add("idUser", IdUser.Text);
+                parametros.Add("usuario", Usuario.Text);
+                parametros.Add("password", Password.Text)
 
                 UserProfile.UploadValues("http://134.209.220.83/proyecto/", "Post", parametros);
                 await DisplayAlert("Alerta", "Datos Ingresados", "Ok");
+
             }
             catch (Exception ex)
             {
-
                 await DisplayAlert("Alerta", "ERROR" + ex.Message, "Ok");
             }
 
         }
 
-        private async void btnRegresar_Clicked(object sender, EventArgs e)
+        private void btnRegresar_Clicked(object sender, EventArgs e)
         {
-
-            await Navigation.PushAsync(new Login());
 
         }
     }
