2d2b756ec0f48e97a8be194238234cf2c8e64110
BYRON SANTIAGO <byronresort@hotmail.com>
2020-11-25 22:07:19 -0500

    conexion servidor user.xaml.cs

--- ProyectoFinal/ProyectoFinal/Login.xaml.cs
+++ ProyectoFinal/ProyectoFinal/Login.xaml.cs
@@ -41,9 +41,11 @@ namespace ProyectoFinal
             
         }
 
-        private  void btnRegistrar_Clicked(object sender, EventArgs e)
+        private  async void btnRegistrar_Clicked(object sender, EventArgs e)
         {
-            
+
+            await Navigation.PushAsync(new User());
+
         }
     }
 }
